bb03e9d218c0518eea3252a91f5ffed4bc2b09ec
EggsArntMemes <135548594+EggsArntMeme@users.noreply.github.com>
2025-04-07 11:02:11 -0600

    added async and await

--- js/api.js
+++ js/api.js
@@ -1,7 +1,7 @@
-function getScore() {
+async function getScore() {
     // const BASE_URL = 'http://localhost:3000';
     const BASE_URL = 'https://java-sweeper.netlify.app'
-    return fetch(`${BASE_URL}/score.json`)   
+    await fetch(`${BASE_URL}/score.json`)   
         .then((response) => {
             return response.json();
         })
